748e34d8eb73d4a545d65c73ac0ac6e0aae28cee
ethannewton500 <109337928+ethannewton500@users.noreply.github.com>
2024-03-26 13:14:36 -0700

    working tutorial api calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { inject } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'homework3';
+export class AppComponent implements OnInit{
+  
+httpClient = inject(HttpClient)
+data: any = [];
+
+ngOnInit(): void {
+  this.fetchData();
+}
+
+fetchData() {
+  this.httpClient.get('https://jsonplaceholder.typicode.com/posts').subscribe((data) => {
+    console.log(data);
+    this.data = data;
+  });
+}
 }
